48b65f9e3953b3ec374fc914cd241ecdf28a4480
mozzam123 <mozzam607@gmail.com>
2023-11-01 10:16:02 +0530

    corrected login page with csrf token

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -7,4 +7,9 @@ def Register(request):
     return render(request, 'register.html')
 
 def Login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get('loginpassword')
+
+        print(username, password)
     return render(request, 'login.html')
