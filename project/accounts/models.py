949a45d8fe0ac754eed3b5274cdf40dda3dffa0b
mozzam123 <mozzam07@gmail.com>
2023-11-18 20:03:41 +0530

    added base model

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Strategy(models.Model):
+class TimeStampedModel(models.Model):
+    createddate = models.DateTimeField(
+        auto_now_add=True, db_column='createddate')
+    updateddate = models.DateTimeField(auto_now=True, db_column='updateddate')
+
+    class Meta:
+        abstract = True
+
+
+class Strategy(TimeStampedModel):
     username = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
 
@@ -11,5 +20,8 @@ class Strategy(models.Model):
 
     class Meta:
         db_table = "Strategy"
+
+# class NetPosition(models.Model):
+
 
     
